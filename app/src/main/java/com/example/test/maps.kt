3c50b089b5cc07b562ab65830c81e044dbe491c7
swinaaron <102567604@student.swin.edu.au>
2021-09-15 20:55:17 +1000

    Added Map

--- app/src/main/java/com/example/test/maps.kt
+++ app/src/main/java/com/example/test/maps.kt
@@ -1,11 +1,34 @@
 package com.example.test
 
+import android.app.Notification
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.maps.Style.MAPBOX_STREETS
+import kotlinx.android.synthetic.main.activity_maps.*
+import com.mapbox.mapboxsdk.maps.Style.MAPBOX_STREETS
+
 
 class maps : AppCompatActivity() {
+
+    private var mapView: MapView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
         setContentView(R.layout.activity_maps)
+
+        mapView = findViewById<MapView>(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+
+            }
+
+        }
     }
 }
